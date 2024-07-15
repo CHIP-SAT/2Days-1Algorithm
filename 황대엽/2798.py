640d325021f4ede8cd8f59e46117392638f6f5f6
daeyeop24 <dy08291@naver.com>
2024-07-15 18:45:31 +0900

    백준 1018번 문제 정답

---
+++ 황대엽/2798.py
@@ -0,0 +1,11 @@
+#// 20:00 시작
+#// 00:00 끝
+import random
+N=random.randint(3,100)
+M=random.randint(10,300000)
+print(N, end=' ')
+print(M)
+for i in range(N):
+    C=random.randint(1,M)
+    print(C,end=' ')
+print(d)
