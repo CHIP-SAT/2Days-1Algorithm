4c3a25d3b0b81602fe927ebea9d99770ba0f1dc7
ppaksu <smreo082625@gmail.com>
2024-07-27 23:37:01 +0900

    Create 2164.c

---
+++ 박수현/2164.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int N;
+    scanf("%d", &N);
+
+    int list[500000];
+    int front = 0, rear = N - 1;
+
+    for (int i = 0; i < N; i++) {
+        list[i] = i + 1;
+    }
+
+    while (front != rear) {
+        front = (front + 1) % N;
+        rear = (rear + 1) % N;
+        list[rear] = list[front];
+        front = (front + 1) % N;
+    }
+
+    printf("%d\n", list[front]);
+
+    return 0;
+}
